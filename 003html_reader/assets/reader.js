521c8fb3c175f6da31c97c2452036a8487c093c4
gagagixi01 <xiangjiawei@qq.com>
2026-06-11 21:13:49 +0800

    update

--- 003html_reader/assets/reader.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelector(".menu-button");
-const sidebar = document.querySelector("#sidebar");
-
-if (button && sidebar) {
-  button.addEventListener("click", () => {
-    const isOpen = sidebar.classList.toggle("open");
-    button.setAttribute("aria-expanded", String(isOpen));
-  });
-}
